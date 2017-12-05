655c3115e6a1ed4006c1b03a93b2575ab2c4b6eb
Simon Wilson <simon.wilson@digital.cabinet-office.gov.uk>
2017-12-05 08:33:18 +0000

    Day 3 part 1

---
+++ day03.rb
@@ -0,0 +1,40 @@
+$input = 289326
+
+puts $input
+
+# First half
+
+$total = 1
+
+puts "Level 1: 1 to 1, 0 per side"
+
+$level = 2
+
+while $total < $input do
+  start = $total + 1
+  side = 2 * ($level-1)
+  finish = start + (4 * side) - 1
+  #puts "Level #{$level}: #{start} to #{finish}, #{side} per side"
+
+  $total = finish
+  $level += 1
+end
+
+puts $total
+
+puts "Level 270: 288370 to 290521, 538 per side"
+
+puts "Corners:"
+puts "#{288370} is at (269, -#{536/2})"
+topRightCorner = 288370 + 538 - 1
+puts "#{topRightCorner} is at (269, #{538/2})"
+puts "#{$input} is #{$input - topRightCorner} further down the edge"
+puts "#{$input} is at (#{(538/2) - $input + topRightCorner}, 269)"
+
+puts "#{288370 + 538 + 538 - 1} is at (#{269 - 538}, #{538/2})"
+puts "#{288370 + 538 + 538 + 583 - 1} is at (#{269 - 538}, -#{538/2})"
+puts "#{288370 + 538 + 538 + 538 + 538 - 1} is at (#{269}, -#{538/2})"
+
+puts "distance: #{269 + 150}"
+
+# second half
